dee15fdccf26d02d8c9887a4394676a8b1eaf1cf
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-11-21 21:33:05 +0100

    added routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../widgets/Navbar/ui/Navbar";
+import MoviesPage from "../pages/MoviesPages/ui/MoviesPage";
+import PeoplePage from "../pages/PeoplePage/ui/PeoplePage";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <p>Movies Browser</p>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MoviesPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/people" element={<PeoplePage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
