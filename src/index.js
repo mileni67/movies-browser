38c4c2c2bad62db6dee92e6a651c9d606e82bfa2
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-11-21 21:31:40 +0100

    added theme and browserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./shared/styles/theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
