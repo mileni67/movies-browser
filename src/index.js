63d5dc85812290be982eb777f4ea0313fd86bbe1
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-11-24 21:09:53 +0100

    added basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter>
+    <BrowserRouter basename="/movies-browser">
       <App />
     </BrowserRouter>
   </ThemeProvider>
