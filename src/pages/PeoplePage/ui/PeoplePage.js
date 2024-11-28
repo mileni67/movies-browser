fd69d55aff4820616f7bf3f535150ab07e1724d1
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-11-28 21:50:37 +0100

    added Movies and People page (Api)

--- src/pages/PeoplePage/ui/PeoplePage.js
+++ src/pages/PeoplePage/ui/PeoplePage.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import tmdbApi from "../../../shared/api/tmdb";
 
 const PeoplePage = () => {
-  return <h1>Welcome to the People Page!</h1>;
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    tmdbApi
+      .get("/person/popular")
+      .then((response) => setPeople(response.data.results))
+      .catch((error) => console.error("error loading people", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Popular People</h1>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PeoplePage;
