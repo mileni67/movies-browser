7aab76944e796c337b35d0af8c9f61ade8f58333
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-12-05 21:33:21 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./shared/styles/theme";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter basename="/movies-browser">
+    <HashRouter basename="/movies-browser">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>
 );
